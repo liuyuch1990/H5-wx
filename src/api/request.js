b277b35092e88d0703c13e6d337611534e68cdbf
liuyuch1990 <liuyuch1990@163.com>
2020-07-22 21:05:41 +0800

    add skip login function

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-
+import router from '../router'
+import store from '../store'
 axios.defaults.withCredentials = true;
 // 创建axios实例
 const service = axios.create({
@@ -20,10 +21,10 @@ service.interceptors.request.use(config => {
 // response拦截器
 service.interceptors.response.use(response => {
   //拦截session超时
-  let isTimeout = (response && response.data && (response.data.code === 201 || response.data.code === 1006 || response.data.code === 1007 || response.data.code === 1008 || response.data.code === 1013));
+  let isTimeout = (response && (response.data.code === '0202' || response.data.code === 1006 || response.data.code === 1007 || response.data.code === 1008 || response.data.code === 1013));
   if (isTimeout) {
     store.commit('common/setUser', '');
-    router.replace({path: '/'});
+    router.replace({path: '/login'});
   }
   return response;
 }, error => {
